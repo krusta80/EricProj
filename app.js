35a25a7f80c4d9f4dca19b4ad9ae9e6d42f821fa
John Gruska <johngruska@Johns-MBP-3.home>
2016-03-04 09:29:31 -0500

    Basic routes and bootstrap templates created

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ var express = require('express');
 var morgan = require('morgan');
 var swig = require('swig');
 var bodyParser = require('body-parser');
+var router = require('./routes/');
+
+var jsonParser = bodyParser.json();
+var urlParser = bodyParser.urlencoded({extended:false});
+
 var app = express();
 
 var port = 3000;
-
 console.log("Starting server on port "+port);
 app.listen(port);
 
+app.engine('html',swig.renderFile);
+app.set('view engine','html');
+app.set('views', __dirname + '/views/');
+swig.setDefaults({cache : false});
+
 app.use(express.static(__dirname + "/public"));
 app.use(morgan('combined'));
-var jsonParser = bodyParser.json();
-var urlParser = bodyParser.urlencoded({extended:false});
+app.use('/',router);
